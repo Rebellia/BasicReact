d2a2a2694771f72502fefd41f159a810b2383ae3
Rebellia <sleepy.wrath@gmail.com>
2021-11-10 14:12:02 -0700

    view changes based on which button is clicked

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,9 @@
 import React, { Component } from "react";
 import "../styles/App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Options from "./options";
 import OneOption from "./OneOption";
 import Movies from "./movies";
 import InsertMovie from "./InsertMovie";
-
-/* function App() {
-  return(
-    <>
-      <OneOption title="search"/>
-      <OneOption title="view"/>
-      <OneOption title="add"/>
-    </>
-  ) ;
-} 
-
-export default App;*/
-
 
 export default class App extends Component {
   constructor() {
@@ -27,7 +13,7 @@ export default class App extends Component {
     }
   }
 
-  changeMode = (newMode) => {
+  setView = (newMode) => {
     this.setState({mainViewMode: newMode});
   }
 
@@ -49,9 +35,9 @@ export default class App extends Component {
 
     return(
       <>
-        <OneOption title="search" modeChanger={this.changeMode}/>
-        <OneOption title="view" modeChanger={this.changeMode}/>
-        <OneOption title="add" modeChanger={this.changeMode}/>
+        <OneOption title="search" setView = {this.setView}/>
+        <OneOption title="view" setView = {this.setView}/>
+        <OneOption title="add" setView = {this.setView}/>
         { view() }
       </>      
     );
--- frontend/src/components/OneOption.js
+++ frontend/src/components/OneOption.js
@@ -3,20 +3,22 @@ import Movies from "./movies";
 import InsertMovie from "./InsertMovie";
 
 export default class OneOption extends Component {
-    constructor() {
-        super();
-        this.state = {
-            buttonClicked: false,
-            myChoice: '',
-        }
+    constructor(props) {
+        super(props);
+        this.handleClick.bind(this);
+    }
+
+    handleClick = () => {
+        //this.props.changeMode({this.props.title});
+        const title = this.props.title;
+        this.props.setView(title);
     }
   
     render = () => {
         return(
             <>
-                <button className="btn btn-primary" onClick={this.nextMove}>{this.props.title}</button>
-            </>
-            
+                <button className="btn btn-primary" onClick={this.handleClick}>{this.props.title}</button>
+            </> 
         );
     }
 }
